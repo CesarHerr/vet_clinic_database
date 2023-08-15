d0bdff945b154a11a278667001566c9c811ded7d
Cesar Herrera <cesarherr321@gmail.com>
2023-08-15 00:19:18 -0400

    Add data, queries and schema to the project

--- queries.sql
+++ queries.sql
@@ -15,3 +15,67 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+begin;
+
+  update animals set species = 'unspecified';
+
+  select * from animals;
+
+rollback;
+
+
+select * from animals;
+
+
+begin;
+
+  update animals set species = 'digimon' where name like '%mon';
+
+  update animals set species = 'pokemon' where species is null;
+
+  select * from animals;
+
+commit;
+
+
+select * from animals;
+
+
+begin;
+
+  delete from animals;
+
+rollback;
+
+
+select * from animals;
+
+
+begin;
+
+  delete from animals where date_of_birth > '2022-01-01';
+
+  savepoint sp_animal;
+
+  update animals set weight_kg = weight_kg * -1;
+
+  rollback to sp_animal;
+
+  update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+
+commit;
+
+
+select count(*) from animals;
+
+select count(*) from animals where escape_attempts = 0;
+
+select avg(weight_kg) from animals;
+
+select neutered, sum(escape_attempts) from animals group by neutered; 
+
+select species, min(weight_kg), max(weight_kg) from animals group by species;
+
+select species, avg(escape_attempts) from animals where extract(year from date_of_birth) between 1990 and 2000 group by species;
